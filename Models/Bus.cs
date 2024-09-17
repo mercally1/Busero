df4200992f31d6b65b7ef390d2fd7897db6f1af5
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-17 17:20:12 -0600

    building classes

---
+++ Models/Bus.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Busero.Models;
+
+public class Bus
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string? Placa { get; set; }
+
+    [Required]
+    [MaxLength(5)]
+    [Display (Name = "Año")]
+    public string? Anho { get; set; }
+
+    [Required]
+    [MaxLength(35)]
+    [Display (Name = "Marca")]
+    public string? Brand { get; set; }
+
+    [Required]
+    [MaxLength(25)]
+    [Display (Name = "Modelo")]
+    public string? Model { get; set; }
+
+    [Required]
+    [Display (Name = "Conductor")]
+    public int? DriverId { get; set; }
+
+    [ForeignKey("DriverId")]
+    public virtual Driver? Driver { get; set; }
+}
---
+++ Models/Driver.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Busero.Models;
+
+public class Driver
+{
+    [Key]
+    public int DriverId { get; set; }
+
+    [Required]
+    [Display (Name = "Nombre")]
+    public string? Name { get; set; }
+
+    [Required]
+    [Display (Name = "Apellido")]
+    public string? Lastname { get; set; }
+
+    [Required]
+    public string? Licencia { get; set; }
+
+    [Required]
+    [Display (Name = "Contacto")]
+    public string? Contact { get; set; }
+}
